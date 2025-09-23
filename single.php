089adf336e3be43cf4b1cab5df43d43b53dfe2d7
Darya Semina <darina1970@mail.ru>
2025-09-23 14:12:08 +0700

    feat: add single.php for article

---
+++ single.php
@@ -0,0 +1,112 @@
+<?php
+get_header();
+?>
+
+<main>
+    <section class="article section-common" id="article">
+        <div class="article__wrapper container">
+            <div class="blog-article__image">
+                <img src="./assets/icons/monogram.png" alt="monogram">
+            </div>
+            <div class="article__slider">
+                <div class="article__main-image">
+                    <img id="current" src="./assets/images/product-page/quest1-1.webp" alt="Main image">
+                </div>
+                <div class="images-slider">
+                    <img src="./assets/images/product-page/quest1-1.webp" class="img active" alt="img">
+                    <img src="./assets/images/product-page/quest1-2.webp" class="img" alt="img">
+                    <img src="./assets/images/product-page/quest1-3.webp" class="img" alt="img">
+                    <img src="./assets/images/product-page/quest1-4.webp" class="img" alt="img">
+                </div>
+                <div class="slider-arrows">
+                    <button class="left-arrow"><img src="./assets/icons/left-arrow.png" alt="left-arrow"></button>
+                    <button class="right-arrow"><img src="./assets/icons/right-arrow.png"
+                            alt="right-arrow"></button>
+                </div>
+            </div>
+            <div id="lightbox" class="lightbox">
+                <span class="btn-close">&times;</span>
+                <span class="btn-prev">&#10094;</span>
+                <img class="lightbox-content" id="lightbox-img" alt="lightbox">
+                <span class="btn-next">&#10095;</span>
+            </div>
+            <div class="blog__item-descr">
+                <a href="article.html">
+                    <h3 class="blog__item-title">Deciphering an Ancient Map</h3>
+                </a>
+                <p class="blog__item-date">August 28, 2025</p>
+                <div class="blog__item-text">
+                    <p>Faded ink, torn edges, and cryptic symbols… The map seems to whisper
+                        secrets of places long forgotten. Every mark invites you to look closer, to connect clues
+                        and
+                        uncover what lies beyond the obvious. Faded ink, torn edges, and cryptic symbols… The map
+                        seems
+                        to whisper secrets of places long forgotten. Every mark invites you to look closer, to
+                        connect
+                        clues and uncover what lies beyond the obvious.Faded ink, torn edges, and cryptic symbols…
+                        The
+                        map seems to whisper secrets of places long forgotten. Every mark invites you to look
+                        closer, to
+                        connect clues and uncover what lies beyond the obvious.Faded ink, torn edges, and cryptic
+                        symbols… The map seems to whisper secrets of places long forgotten. Every mark invites you
+                        to
+                        look closer, to connect clues and uncover what lies beyond the obvious.Faded ink, torn
+                        edges,
+                        and cryptic symbols… The map seems to whisper secrets of places long forgotten. Every mark
+                        invites you to look closer, to connect clues and uncover what lies beyond the obvious.Faded
+                        ink,
+                        torn edges, and cryptic symbols… The map seems to whisper secrets of places long forgotten.
+                        Every mark invites you to look closer, to connect clues and uncover what lies beyond the
+                        obvious.Faded ink, torn edges, and cryptic symbols…
+                        The map seems to whisper secrets of places long forgotten. Every mark invites you to look
+                        closer, to connect clues and uncover what lies beyond the obvious.Faded ink, torn edges, and
+                        cryptic symbols…</p>
+                    <p>The map seems to whisper secrets of places long forgotten. Every mark invites you to look
+                        closer, to connect clues and uncover what lies beyond the obvious.Faded ink, torn edges, and
+                        cryptic symbols… The map seems to whisper secrets of places long forgotten. Every mark
+                        invites
+                        you to look closer, to connect clues and uncover what lies beyond the obvious.Faded ink,
+                        torn
+                        edges, and cryptic symbols… The map seems to whisper secrets of places long forgotten. Every
+                        mark invites you to look closer, to connect clues and uncover what lies beyond the
+                        obvious.Faded
+                        ink, torn edges, and cryptic symbols… The map seems to whisper secrets of places long
+                        forgotten.
+                        Every mark invites you to look closer, to connect clues and uncover what lies beyond the
+                        obvious.Faded ink, torn edges, and cryptic symbols… The map seems to whisper secrets of
+                        places
+                        long forgotten. Every mark invites you to look closer, to connect clues and uncover what
+                        lies
+                        beyond the obvious.</p>
+                    <p>Faded ink, torn edges, and cryptic symbols… The map seems to whisper secrets of places long
+                        forgotten. Every mark invites you to look closer, to connect clues and uncover what lies
+                        beyond
+                        the obvious. Faded ink, torn edges, and cryptic symbols… The map seems to whisper secrets of
+                        places long forgotten. Every mark invites you to look closer, to connect clues and uncover
+                        what
+                        lies beyond the obvious. Faded ink, torn edges, and cryptic symbols… The map seems to
+                        whisper
+                        secrets of places long forgotten. Every mark invites you to look closer, to connect clues
+                        and
+                        uncover what lies beyond the obvious. Faded ink, torn edges, and cryptic symbols… The map
+                        seems
+                        to whisper secrets of places long forgotten. Every mark invites you to look closer, to
+                        connect
+                        clues and uncover what lies beyond the obvious. Faded ink, torn edges, and cryptic symbols…
+                        The
+                        map seems to whisper secrets of places long forgotten. Every mark invites you to look
+                        closer, to
+                        connect clues and uncover what lies beyond the obvious.</p>
+                </div>
+            </div>
+            <a href="blog.html" class="blog__link">
+                <span>BACK TO BLOG</span>
+                <img src="./assets/icons/learn-more-arrow.svg" alt="arrow right">
+            </a>
+        </div>
+    </section>
+</main>
+
+<?php
+get_footer();
+?>
