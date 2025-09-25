b9e5e6fd7b3e2f0031912b70e0c5c12215cdf106
Darya Semina <darina1970@mail.ru>
2025-09-25 14:10:46 +0700

    feat: add functions and page for cart and checkout

---
+++ page.php
@@ -0,0 +1,16 @@
+<?php
+get_header();
+?>
+
+<main class="site-main">
+    <div class="container">
+        <?php
+        while (have_posts()) : the_post();
+            the_content();
+        endwhile;
+        ?>
+    </div>
+</main>
+
+<?php
+get_footer();
